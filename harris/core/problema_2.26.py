6a1d17225d972d9b029b4bc2f6ff3aff15ebe96f
Fernando Henrique Fraga <f.henrique.fraga@gmail.com>
2020-01-07 16:06:11 -0300

    inclusion of output for pdf plotting

--- harris/core/problema_2.26.py
+++ harris/core/problema_2.26.py
@@ -2,32 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def pratos_teoricos(A, B, C, u):
-    """Calcula a altura dos pratos teóricos presentes
-    em uma coluna cromatografica
+def plates_theorical(A, B, C, u):
+    """Calculates the height of the theoretical plates present in a chromatographic column
 
     Parameters
     ----------
     A : float
-        alargamento de picos por caminhos preferenciais
+        peak widening by preferred paths
     B :float
-        difusão do soluto na fase móvel
+        solute diffusion in the mobile phase
     C : float
-        difusão do soluto na fase móvel
+        solute diffusion in the stationary phase
     u : float
-        velocidade do gás de arraste
+        carrier gas speed
 
     Returns
     -------
     float
-        altura equivalente a um prato teórico.
+        equivalent height of a theoretical plate.
     """
     H = A + (B / u) + (C * u)
     return(H)
 
 
 if __name__ == '__main__':
-    H = pratos_teoricos(1.65, 25.8, 0.0236, np.arange(4, 102, 2))
-    grafico = plt.plot(H, np.arange(4,102,2))
-    print(H, grafico)
-    
+    H = plates_theorical(1.65, 25.8, 0.0236, np.arange(4, 102, 2))
+    plt.figure(1, figsize=(6, 4))
+    plt. plot(H, np.arange(4, 102, 2))
+    plt.xlabel('Height of theoretical plates(mm)')
+    plt.ylabel('Carrier gas speed(mL/min)')
+
+    # save plot to file
+    plt.savefig("Van Deemter's equation.pdf")
+
+ # display plot on screen
+    plt.show()
+    print(H)
