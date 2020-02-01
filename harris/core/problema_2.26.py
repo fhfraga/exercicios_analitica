760506c32a9af00ed091d597c4047e4c51296904
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-02-01 15:19:33 -0300

    Plot label fixes, tight layout and comment print(H)

--- harris/core/problema_2.26.py
+++ harris/core/problema_2.26.py
@@ -28,15 +28,17 @@ def plates_theorical(A, B, C, u):
 
 if __name__ == '__main__':
     H = plates_theorical(1.65, 25.8, 0.0236, np.arange(4, 102, 2))
+
     plt.figure(1, figsize=(6, 4))
     plt.style.use('seaborn-whitegrid')
     plt.plot(H, np.arange(4, 102, 2))
-    plt.xlabel('Height of theoretical plates(mm)')
-    plt.ylabel('Carrier gas speed(mL/min)')
+    plt.xlabel('Height of theoretical plates / (mm)')
+    plt.ylabel('Carrier gas speed / (mL/min)')
+    plt.tight_layout()  # avoids cutting labels depending on style
 
     # save plot to file
     plt.savefig("Van Deemter's equation.pdf")
 
     # display plot on screen
     plt.show()
-    print(H)
+    # print(H)  # activate for values
